605d121f2dc9c21d221799a9bc6cb1b373d9431b
Aubrey Graves <grav0165@gmail.com>
2023-06-16 19:51:43 -0500

    created call to server to get history. Created post to server to request math operation. Created equals button listener and handler

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,38 +1,86 @@
 $(document).ready(onReady);
 
-let operators = ""
+let operator = ""
 
 function onReady() {
-console.log('jQuery is loaded!');
+    console.log('jQuery is loaded!');
 
-('#plus-btn').on('click', operatorPlus);
-('#minus-btn').on('click', operatorMinus);
-('#mutliply-btn').on('click', operatorMultiply);
-('#divide-btn').on('click', operatorDivide);
+    getPreviousMathAnswers();
+
+    $('#plus-btn').on('click', operatorPlus);
+    $('#minus-btn').on('click', operatorMinus);
+    $('#mutliply-btn').on('click', operatorMultiply);
+    $('#divide-btn').on('click', operatorDivide);
+    $('#equals-btn').on('click', solveMathEquation);
 
 
+}
 
+
+// Created get function to request from server any saved information
+// TODO - ADD RENDER TO getPreviousMathAnswers
+function getPreviousMathAnswers() {
+    $.ajax({
+        method: 'GET',
+        url: '/savedPreviousMathAnswers'
+    }).then(function(response){
+        console.log('getPreviousMathAnswers function!', response)
+        // INSERT RENDER FUNCTION HERE;
+    }).catch(function(error){
+        alert('Uh oh! You have an error!');
+        console.log('Error server', error);
+    })
+}
+
+function solveMathEquation(event) {
+    console.log('in solveMathEquation');
+    // Preventing default form functions in HTML
+    event.preventDefault();
+    // creating object to package and send to server
+    let mathObjectToSend = {
+        varA: $('#var-a-input').val(),
+        varB: $('#var-b-input').val(),
+        operatorNotation: operator
+    }
+    if(mathObjectToSend.varA === nil || mathObjectToSend.varB === nil) {
+        console.log('Error! Please enter at least one variable');
+    } else {
+        $.ajax({
+            method: 'POST',
+            url: '/savedPreviousMathAnswers',
+            data: mathObjectToSend
+        }).then(function(response) {
+            console.log('Success!', repsonse);
+            getPreviousMathAnswers();
+        }).catch(function(error) {
+            alert('Uh Oh! You have an error!');
+            console.log('Error with post', error);
+        })
+    }
+    // Creating call to empty both input blocks
+    $('#var-a-input').val('');
+    $('#var-b-input').val('');
 }
 
 
 // Creating a different on-click for each type of operation button.
 function operatorPlus() {
-    operators = "plus";
+    operator = "plus";
     console.log('Addition selected')
 }
 
 function operatorMinus(){
-    operators = "minus";
+    operator = "minus";
     console.log('Subtraction selected')
 }
 
 function operatorMultiply(){
-    operators = "multiply";
+    operator = "multiply";
     console.log('Multiplication selected')
 }
 
 function operatorDivide() {
-    operators = "divide";
+    operator = "divide";
     console.log('Division selected')
 }
 
